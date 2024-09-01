028feaa24dc9379a9d1fe10d6fdc92400ee069ee
mohamed ahmed <midoahmed120303@gmail.com>
2024-09-01 16:17:27 +0300

    assigment 1

---
+++ unit-3 embedded_c/lesson1/static library/main.c
@@ -0,0 +1,7 @@
+#include <stdio.h>
+#include"try.h"
+
+int main(void){
+    trial();
+    return 0;
+}
---
+++ unit-3 embedded_c/lesson1/static library/try.c
@@ -0,0 +1,5 @@
+#include<stdio.h>
+
+void trial(void){
+    printf("i'm just trying static library!");
+}
